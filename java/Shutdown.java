ecdd3172aa6fd5e87ca24a45f6b7c371755e1e10
FOXROLONG <longjakiu1@gmail.com>
2026-09-06 16:03:24 +0700

    update

--- java/Shutdown.java
+++ java/Shutdown.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class Shutdown {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-				Runtime run = Runtime.getRuntime();
+				Runtime runtime = Runtime.getRuntime();
 				Process proc = runtime.exec("shutdown -s -t 0");
 	}
 }
