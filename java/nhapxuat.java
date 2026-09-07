0cc80e28c9797c933883203338296ad64062bed9
FOXROLONG <longjakiu1@gmail.com>
2026-09-07 17:46:45 +0700

    update

--- java/nhapxuat.java
+++ java/nhapxuat.java
@@ -1,3 +1,9 @@
 public class nhapxuat {
-    
+    public static void main(String[] args) {
+        System.out.println("nhập số nguyên:");
+        int a;
+        a = Integer.parseInt(System.console().readLine());
+        System.out.println("số vừa nhập là: " + a);
+
+    }
 }
