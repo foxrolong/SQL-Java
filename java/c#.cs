ae28accc02a45f340c4c9e4d1dd30d9c012b0781
FOXROLONG <longjakiu1@gmail.com>
2026-09-09 17:04:02 +0700

    update

---
+++ java/c#.cs
@@ -0,0 +1,9 @@
+using System;
+class hienthi
+{
+    static void Main(string[] args)
+    {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- java/nhapxuat.java
+++ java/nhapxuat.java
@@ -1,9 +1,12 @@
 public class nhapxuat {
     public static void main(String[] args) {
+    try{
         System.out.println("nhập số nguyên:");
         int a;
         a = Integer.parseInt(System.console().readLine());
         System.out.println("số vừa nhập là: " + a);
-
+    } catch (Exception e) {
+        System.out.println("lỗi: vui lòng nhập một số nguyên hợp lệ." + e );
+    }
     }
 }
