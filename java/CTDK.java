51798b20f1a358b380447c238abbcb6061dd027e
foxrolong <longjakiu1@gmail.com>
2026-09-16 21:33:58 +0700

    update

--- java/CTDK.java
+++ java/CTDK.java
@@ -1,13 +1,46 @@
+import java.util.*;
+
 public class CTDK {
     public static void main(String[] args){
+
         double diemTrungBinh = 3.35;
         int diemRenLuyen = 85;
         int soBuoiNghi = 4;
         boolean biKyLuat = false;
         int luaChon = 3;
+        String xepLoai;
 
-        System.out.println((soBuoinghi > 10) ? "sinh viên nghỉ học quá nhiều" : "");
+        
+        //phần 1: if rút gọn sử dụng toán tử 3 ngôi
+        String thongBao = (soBuoiNghi > 10) ? "sinh viên nghỉ học quá nhiều" : "Sinh viên nghỉ: " + soBuoiNghi + " buổi";
+        System.out.println("Thong bao: " + thongBao);
 
+        //phần 2: if...else
+        if (diemTrungBinh >= 2.0){
+            System.out.println("Du dieu kien tiep tuc hoc");
+        }else{
+            System.out.println("Canh Bao Hoc Vu");
+        }
+
+        //phần 3: if...else if...else
+        if(diemTrungBinh>=3.6){
+            xepLoai = "xuat sac";
+        }else if(diemTrungBinh>=3.2){
+            xepLoai = "gioi";
+        }else if(diemTrungBinh>=2.5){
+            xepLoai = "kha";
+        }else if(diemTrungBinh>=2.0){
+            xepLoai = "trung binh";
+        }else{
+            xepLoai = "yeu";
+        }
+
+        System.out.println("Xep loai: " + xepLoai);
+
+        //phan 4 ket hop toan tu logic
+        if(diemTrungBinh>=3.2 && diemRenLuyen>=80 && soBuoiNghi<=10 && !biKyLuat){
+            System.out.println("Du dieu kien nhan hoc bong");
+        }
     }
     
 }
